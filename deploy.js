48fb608774c6e6ea1039b9e2692b12542cc36543
839900146 <839900146@qq.com>
2021-02-22 11:46:35 +0800

    自动部署

--- deploy.js
+++ deploy.js
@@ -31,11 +31,22 @@ function uploadFile(filepath) {
 
     console.log('开始部署至服务器');
 
-    axios.post(remoteServer, form, { headers }).then(res => {
+    axios.post(remoteServer, form, {
+        headers,
+        onUploadProgress: e => {
+            const { loaded, total } = e;
+            // 使用本地 progress 事件
+            if (e.lengthComputable) {
+                let progress = loaded / total * 100;
+                console.log(progress);
+            }
+        }
+    }).then(res => {
         if (res.data.code === 200 && res.data.state === true) {
             console.log('博客已成功部署至服务器');
             fs.unlinkSync(filepath);
             console.log('已删除blog.zip');
+            console.log('已删除public');
         } else {
             console.log(`博客更新失败，${res.data.message}`);
         }
